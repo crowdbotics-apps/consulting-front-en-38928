09db9358068145e8b8b5e96ff14fe9ec1e424ee7
Crowdbotics <team@crowdbotics.com>
2023-02-15 22:13:40 +0000

    Initial commit.

---
+++ consulting_front_en_38928/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for consulting_front_en_38928 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'consulting_front_en_38928.settings')
+
+application = get_wsgi_application()
